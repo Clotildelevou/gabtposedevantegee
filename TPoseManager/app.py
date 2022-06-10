f7cf56f1f3a406fb64a5d8523d4ee18053cccc7b
KubbyDev <gbrljorge@gmail.com>
2022-06-10 12:33:20 +0100

    Feat: Videos list query

--- TPoseManager/app.py
+++ TPoseManager/app.py
@@ -1,24 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 import ffmpeg
 
 app = Flask("T-Pose Manager")
-app.config.from_pyfile('config.py')
+
+VIDEOS_DIR = "/home/pi/gabtposedevantegee/tposes"
 
 @app.route("/videos")
 def videos():
-    videosdir = app.config['videos_dir']
     res = []
-
-    filenames = os.listdir(videosdir)
+    filenames = os.listdir(VIDEOS_DIR)
     for name in filenames:
-        path = os.path.join(videosdir, name)
+        path = os.path.join(VIDEOS_DIR, name)
         if not os.path.isfile(path):
             continue
         metadata = ffmpeg.probe(path)["streams"][0]
         res.append({
             'duration':  metadata["duration"],
-            'date': os.path.getmtime(path)
+            'date': os.path.getmtime(path),
+            'id': "0"
         })
+    return jsonify(res)
 
-    return res
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- TPoseManager/config.py
+++
@@ -1,7 +0,0 @@
-"""Flask configuration."""
-
-TESTING = True
-DEBUG = True
-FLASK_ENV = 'development'
-
-videos_dir = '/home/pi/tposes'
